53b9346999069a3b8fa0601780f682876f09ca04
Mario Cao <mario@stampery.co>
2018-10-03 16:47:53 +0200

    ping example

---
+++ README.md
@@ -0,0 +1,7 @@
+# Actix example
+
+Some example to better understand how Actix works. :)
+
+```bash
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,65 @@
-use actix::{Actor, Arbiter, Context, Handler, Message, System};
-use futures::{future, Future};
+use actix::*;
+use futures::Future;
+use std::io;
 
-// this is our Message
-struct SumRequest(usize, usize);
-
-// we have to define the response type for `Sum` message
-impl Message for SumRequest {
-    type Result = usize;
+// Define message
+struct Ping {
+    pub id: usize,
 }
 
-// Actor definition
-struct Summator;
+impl Message for Ping {
+    type Result = Result<bool, io::Error>;
+}
 
-impl Actor for Summator {
+// Define actor
+struct MyActor;
+
+// Provide Actor implementation for our actor
+impl Actor for MyActor {
     type Context = Context<Self>;
+
+    fn started(&mut self, _ctx: &mut Context<Self>) {
+        println!("Actor is alive");
+    }
+
+    fn stopped(&mut self, _ctx: &mut Context<Self>) {
+        println!("Actor is stopped");
+    }
 }
 
-// now we need to define `MessageHandler` for the `Sum` message.
-impl Handler<SumRequest> for Summator {
-    type Result = usize;   // <- Message response type
+/// Define handler for `Ping` message
+impl Handler<Ping> for MyActor {
+    type Result = Result<bool, io::Error>;
 
-    fn handle(&mut self, msg: SumRequest, _ctx: &mut Context<Self>) -> Self::Result {
-        msg.0 + msg.1
+    fn handle(&mut self, msg: Ping, _ctx: &mut Context<Self>) -> Self::Result {
+        println!("Ping received: {:?}", msg.id);
+        System::current().stop();
+
+        Ok(true)
     }
 }
 
 fn main() {
     let sys = System::new("test");
 
-    let addr = Summator.start();
-    let res = addr.send(SumRequest(10, 5));  // <- send message and get future for result
-    
-    Arbiter::spawn(res.then(|res| {
-        match res {
-            Ok(result) => println!("SUM: {}", result),
-            _ => println!("Something wrong"),
-        }
-        
-        System::current().stop();
-        future::result(Ok(()))
-    }));
+    // Start MyActor in current thread
+    let addr = MyActor.start();
+
+    // Send Ping message.
+    // send() message returns Future object, that resolves to message result
+    let result = addr.send(Ping { id: 10 });
+
+    // spawn future to reactor
+    Arbiter::spawn(
+        result
+            .map(|res| match res {
+                Ok(result) => println!("Got result: {}", result),
+                Err(err) => println!("Got error: {}", err),
+            })
+            .map_err(|e| {
+                println!("Actor is probably died: {}", e);
+            }),
+    );
 
     sys.run();
 }
